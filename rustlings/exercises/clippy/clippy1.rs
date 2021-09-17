efe636699bc158ce1543dd86e374220ef11c3c87
Axel Berardino <axel@zen.ly>
2021-09-17 16:39:03 +0200

    add rustlings

---
+++ rustlings/exercises/clippy/clippy1.rs
@@ -0,0 +1,16 @@
+// clippy1.rs
+// The Clippy tool is a collection of lints to analyze your code
+// so you can catch common mistakes and improve your Rust code.
+//
+// For these exercises the code will fail to compile when there are clippy warnings
+// check clippy's suggestions from the output to solve the exercise.
+// Execute `rustlings hint clippy1` for hints :)
+
+fn main() {
+    const ERROR_MARGIN: f64 = 0.0001;
+    let x = 1.2331f64;
+    let y = 1.2332f64;
+    if (y - x).abs() > ERROR_MARGIN {
+        println!("Success!");
+    }
+}
